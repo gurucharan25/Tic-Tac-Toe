595f78be6b78d0e9980f8d42922d24c826cc97d2
gurucharan25 <thonangigurucharan@gmail.com>
2026-04-28 12:53:51 +0530

    Add files via upload

---
+++ TicTacToeUC10.java
@@ -0,0 +1,25 @@
+public class TicTacToe {
+
+    static char[][] board = {
+            {'X', 'O', 'X'},
+            {'O', 'X', 'O'},
+            {'O', 'X', 'X'}
+    };
+
+
+    public static void main(String[] args) {
+        System.out.println(isDraw());
+    }
+
+
+    static boolean isDraw() {
+        for (int r = 0; r < 3; r++) {
+            for (int c = 0; c < 3; c++) {
+                if (board[r][c] == '-') {
+                    return false; // empty cell found, not a draw
+                }
+            }
+        }
+        return true; // no empty cells left
+    }
+}
